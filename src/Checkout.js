46c4acf5534a098cb69ab71afa04bef5fd602255
nick1498 <niacono14@gmail.com>
2021-08-09 15:29:10 +1000

    more checkout page changes, added context api

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from "./Subtotal";
 
 function Checkout() {
     return (
@@ -13,7 +14,7 @@ function Checkout() {
                 </div>
             </div>
             <div className="checkout__right">
-                <h2>The Subtotal component</h2>
+                <Subtotal />
             </div>
         </div>
     )
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from './StateProvider';
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log("this is basket", basket);
+
+    const addToBasket = () => {
+        //dispatch the item to the datalayer ie basket
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -21,9 +41,9 @@ function Product({ title, image, price, rating }) {
                 </div>
             </div>
 
-            <img src="https://m.media-amazon.com/images/I/4151dD06RqL._AC_SY230_.jpg" />
+            <img src={image} />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
