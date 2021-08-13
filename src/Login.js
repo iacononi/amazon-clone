ddaafd616318eb99405d862c5802f7a4d17ef92d
sanskar915 <42159911+sanskar915@users.noreply.github.com>
2021-08-13 18:33:10 +1000

    Front end login

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
+import { Link } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+    }
+
+    const register = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div className='Login'>
-            <img src='https://en.wikipedia.org/wiki/File:Amazon_logo.svg'/>
+        <div className='login'>
+            <Link to='/'>
+                <img className="login__logo"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                />
+            </Link>
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} 
+                    onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} 
+                    onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' 
+                    onClick={signIn} className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in to this fake amazon clone you hereby agree to give me access to you pornhub premium account 
+                </p>  
+            </div>
         </div>
     )
 }
