f5e6a3a9689aea103fb18023f7ffe60a7b6899a7
nick1498 <niacono14@gmail.com>
2021-08-30 11:12:04 +1000

    updated readme and gitignore

--- src/Login.js
+++ src/Login.js
@@ -11,22 +11,22 @@ function Login() {
     const signIn = e => {
         e.preventDefault();
         auth.signInWithEmailAndPassword(email, password)
-        .then(auth => {
-            history.push('/')
-        })
-        .catch(error => alert(error.message))
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
     }
 
     const register = e => {
         e.preventDefault();
         auth.createUserWithEmailAndPassword(email, password)
-        .then((auth) => {
-            // it successfully created a new user with email and password
-            if (auth) {
-                history.push('/')
-            }
-        })
-        .catch(error => alert(error.message))
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
 
     }
 
@@ -34,28 +34,28 @@ function Login() {
         <div className='login'>
             <Link to='/'>
                 <img className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
                 />
             </Link>
             <div className='login__container'>
                 <h1>Sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' value={email} 
-                    onChange={e => setEmail(e.target.value)} />
+                    <input type='text' value={email}
+                        onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' value={password} 
-                    onChange={e => setPassword(e.target.value)} />
+                    <input type='password' value={password}
+                        onChange={e => setPassword(e.target.value)} />
 
-                    <button type='submit' 
-                    onClick={signIn} className='login__signInButton'>Sign In</button>
+                    <button type='submit'
+                        onClick={signIn} className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
-                   By singing up, you agree to the website terms and conditions
-                </p> 
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button> 
+                    By singing up, you agree to the website terms and conditions
+                </p>
+                <button onClick={register} className='login__registerButton'>Create your Account</button>
             </div>
         </div>
     )
